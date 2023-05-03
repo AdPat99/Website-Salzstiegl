ea88809c5b0c05b31d89623be981165f30317b56
AdPat99 <97840771+AdPat99@users.noreply.github.com>
2023-05-03 14:01:47 +0200

    Testing page transition

---
+++ default_site/Transition.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+    const anchors = document.querySelectorAll('a');
+    const transElem = document.querySelector('.transition');
+  
+    setTimeout(() => {
+      transElem.classList.remove('is-active');
+    }, 500);
+  
+    for (let i = 0; i < anchors.length; i++) {
+      const anchor = anchors[i];
+  
+      anchor.addEventListener('click', e => {
+        e.preventDefault();
+        let target = e.target.href;
+  
+        console.log(transElem);
+  
+        transElem.classList.add('is-active');
+  
+        console.log(transElem);
+  
+        setInterval(() => {
+          window.location.href = target;
+        }, 500);
+      })
+    }
+  }
