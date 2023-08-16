2cf7007a065da8e7084e0bfbc8d63f7ec34212ad
Pat <p.adelmann@hotmail.com>
2023-08-16 18:33:36 +0100

    JS - Animation only when Sommer is being clicked

--- default_site/Transition.js
+++ default_site/Transition.js
@@ -1,27 +1,23 @@
 window.onload = () => {
-    const anchors = document.querySelectorAll('a');
-    const transElem = document.querySelector('.transition');
-  
-    setTimeout(() => {
-      transElem.classList.remove('is-active');
+  const transElem = document.querySelector('.transition');
+  const STransElem = document.querySelector('#STrans');
+
+  setTimeout(() => {
+    transElem.classList.remove('is-active');
+  }, 250);
+
+  STransElem.addEventListener('click', e => {
+    e.preventDefault();
+    let target = e.target.href;
+
+    console.log(transElem);
+
+    transElem.classList.add('is-active');
+
+    console.log(transElem);
+
+    setInterval(() => {
+      window.location.href = target;
     }, 250);
-  
-    for (let i = 0; i < anchors.length; i++) {
-      const anchor = anchors[i];
-  
-      anchor.addEventListener('click', e => {
-        e.preventDefault();
-        let target = e.target.href;
-  
-        console.log(transElem);
-  
-        transElem.classList.add('is-active');
-  
-        console.log(transElem);
-  
-        setInterval(() => {
-          window.location.href = target;
-        }, 250);
-      })
-    }
-  }
+  });
+}
