c515c74c2c2ce7fe26033280653cf5171d211e0e
Pat <p.adelmann@hotmail.com>
2023-08-17 18:20:41 +0100

    Basic transition for winter and summer

--- default_site/Transition.js
+++ default_site/Transition.js
@@ -1,6 +1,7 @@
 window.onload = () => {
   const transElem = document.querySelector('.transition');
   const STransElem = document.querySelector('#STrans');
+  const WTransElem = document.querySelector('#WTrans');
 
   setTimeout(() => {
     transElem.classList.remove('is-active');
@@ -13,6 +14,21 @@ window.onload = () => {
     console.log(transElem);
 
     transElem.classList.add('is-active');
+
+    console.log(transElem);
+
+    setTimeout(() => {
+      window.location.href = target; 
+    }, 250);
+  });
+
+  WTransElem.addEventListener('click', e => {
+    e.preventDefault();
+    let target = e.target.href;
+
+    console.log(transElem);
+
+    transElem.classList.add('is-active-2');
 
     console.log(transElem);
 
